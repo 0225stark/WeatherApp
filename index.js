06fd8158b769af539393b00075a8f860f20c7b7c
MIHIR CHOWDHURY <98046589+0225stark@users.noreply.github.com>
2023-08-04 19:29:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path=require("path");
 const app = express();
-
+const fetch = require("node-fetch");
 
 app.use(express.static(path.join(__dirname,"public")));
 app.use(bodyParser.urlencoded({ extended: true })); 
